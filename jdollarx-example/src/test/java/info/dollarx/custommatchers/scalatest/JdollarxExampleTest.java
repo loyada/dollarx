1b510a53c9008f169171bdd6fc20429b04b72b80
Danny Loya <dan.loya@gmail.com>
2015-08-06 08:49:25 -0400

    cleaned jdollarx-example

--- jdollarx-example/src/test/java/info/dollarx/custommatchers/scalatest/JdollarxExampleTest.java
+++ jdollarx-example/src/test/java/info/dollarx/custommatchers/scalatest/JdollarxExampleTest.java
@@ -5,6 +5,7 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.junit.Assert.assertThat;
 
 import info.testtools.jdollarx.InBrowser;
@@ -29,46 +30,12 @@ public class JdollarxExampleTest {
     }
 
     @Before
-    public void goToGoogle(){
+    public void goToGoogle() {
         driver.get("http://www.google.com");
     }
 
     @Test
-     public void googleForAmazonAndVerifyFirstResult(){
-        //Given
-        Path searchFormWrapper = element.that(hasId("searchform")).contains(form);
-        Path google = input.inside(searchFormWrapper);
-
-        //When
-        browser.sendKeys("amazon").to((BasicPath)google);
-
-        //Then
-        Path results = div.that(hasId("search"));
-            Path resultsLink = anchor.inside(results);
-            Path amazonResult = resultsLink.withIndex(0).that(hasTextContaining("amazon.com"));
-            assertThat(amazonResult, isPresentIn(browser));
-    }
-
-    @Test
-    public void googleForAmazonAndFeelingLucky(){
-        //Given
-        Path searchFormWrapper = element.that(hasId("searchform")).contains(form);
-        Path google = input.inside(searchFormWrapper);
-        browser.sendKeys("amazon").to((BasicPath)google);
-
-        //When
-        Path firstSuggestion = listItem.withIndex(0).inside(form);
-        browser.hoverOver(firstSuggestion);
-        Path feelingLucky = anchor.inside(firstSuggestion).withTextContaining("feeling lucky");
-        browser.clickOn(feelingLucky);
-
-        //Then
-        Path amazonMainTitle = title.that(hasTextContaining("amazon")).describedBy("amazon main title");
-        assertThat(amazonMainTitle, isPresentIn(browser));
-    }
-
-    @Test
-    public void googleForAmazonAssertionError1(){
+    public void googleForAmazonAndVerifyFirstResult() {
         //Given
         Path searchFormWrapper = element.that(hasId("searchform")).contains(form);
         Path google = input.inside(searchFormWrapper);
@@ -81,35 +48,70 @@ public class JdollarxExampleTest {
         Path resultsLink = anchor.inside(results);
         Path amazonResult = resultsLink.withIndex(0).that(hasTextContaining("amazon.com"));
         assertThat(amazonResult, isPresentIn(browser));
-        try{
-            assertThat(amazonResult, isPresent(1000).timesIn(browser));
-        } catch (AssertionError e){
-            System.out.println(e);
-        }
     }
 
     @Test
-    public void googleForAmazonAssertionError2(){
+    public void googleForAmazonAndFeelingLucky() {
+        //Given
+        Path searchFormWrapper = element.that(hasId("searchform")).contains(form);
+        Path google = input.inside(searchFormWrapper);
+        browser.sendKeys("amazon").to((BasicPath) google);
+
+        //When
+        Path firstSuggestion = listItem.withIndex(0).inside(form);
+        browser.hoverOver(firstSuggestion);
+        Path feelingLucky = anchor.inside(firstSuggestion).withTextContaining("feeling lucky");
+        browser.clickAt(feelingLucky);
+
+        //Then
+        Path amazonMainTitle = title.that(hasTextContaining("amazon")).describedBy("amazon main title");
+        assertThat(amazonMainTitle, isPresentIn(browser));
+    }
+
+    @Test
+    public void googleForAmazonAssertionError1() {
         //Given
         Path searchFormWrapper = element.that(hasId("searchform")).contains(form);
         Path google = input.inside(searchFormWrapper);
 
         //When
-        browser.sendKeys("amazon").to((BasicPath)google);
+        browser.sendKeys("amazon").to((BasicPath) google);
+
+        //Then
+        Path results = div.that(hasId("search"));
+        Path resultsLink = anchor.inside(results);
+        Path amazonResult = resultsLink.withIndex(0).that(hasTextContaining("amazon.com"));
+        assertThat(amazonResult, isPresentIn(browser));
+        try {
+            assertThat(amazonResult, isPresent(1000).timesIn(browser));
+        } catch (AssertionError e) {
+            e.printStackTrace();
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void googleForAmazonAssertionError2() {
+        //Given
+        Path searchFormWrapper = element.that(hasId("searchform")).contains(form);
+        Path google = input.inside(searchFormWrapper);
+
+        //When
+        browser.sendKeys("amazon").to((BasicPath) google);
 
         //Then
         Path results = div.that(hasId("search"));
         Path resultsLink = anchor.inside(results);
         Path warcraftResult = resultsLink.withIndex(0).that(hasText("for the hord!"));
-        try{
+        try {
             assertThat(warcraftResult, isPresentIn(browser));
-        } catch (AssertionError e){
+        } catch (AssertionError e) {
             System.out.println(e);
         }
     }
 
     @AfterClass
-    public static void teardown(){
+    public static void teardown() {
         driver.quit();
     }
 

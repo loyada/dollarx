06263524b0a0f512209ba3271049ea73e73f52a6
Danny Loya <danny.loya@lab49.com>
2015-09-09 12:41:10 -0400

    minor refactor

--- dollarx-example/src/main/java/info/dollarx/PathToString.scala
+++ dollarx-example/src/main/java/info/dollarx/PathToString.scala
@@ -7,8 +7,8 @@ import info.dollarx.Path._
 
 object PathToString extends App {
   val dialog = div withClass "ui-dialog" withText "foo"
-  val myButton = button withClass("foo") that((has textContaining "submit") and (is not hidden)) inside dialog
-  val myDialog = dialog describedBy("\"create new entity\" dialog")
+  val myButton = button withClass "foo" that((has textContaining "submit") and (is not hidden)) inside dialog
+  val myDialog = dialog describedBy "\"create new entity\" dialog"
   println(dialog)
   println(myButton)
   println(myDialog withClasses ("foo", "bar"))

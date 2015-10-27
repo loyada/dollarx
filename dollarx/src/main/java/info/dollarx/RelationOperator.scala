927d84de35544b2eca77156c3a345ec3126c9c2e
Danny Loya <danny.loya@lab49.com>
2015-10-26 23:47:08 -0400

    Issue #9 - enrich number of siblings, descendants, and children of elements

---
+++ dollarx/src/main/java/info/dollarx/RelationOperator.scala
@@ -0,0 +1,15 @@
+package info.dollarx
+
+
+object RelationOperator extends Enumeration {
+  type RelationOperator = Value
+  val exactly, orMore, orLess = Value
+  def opAsXpathString(relationOperator: RelationOperator) = {
+    val sByOp = Map(exactly -> "=", orMore -> ">=", orLess -> "<=")
+    sByOp(relationOperator)
+  }
+  def opAsEnglish(relationOperator: RelationOperator) = {
+    val sByOp = Map(exactly -> " ", orMore -> " at least ", orLess -> " at most ")
+    sByOp(relationOperator)
+  }
+}

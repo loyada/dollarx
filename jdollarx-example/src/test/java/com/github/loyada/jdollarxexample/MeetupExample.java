c2842717f28cfad775d0e4aeced415d4b28dd1a1
Danny Loya <dan.loya@gmail.com>
2017-01-28 16:06:21 -0500

    add simple intercation example

--- jdollarx-example/src/test/java/com/github/loyada/jdollarxexample/MeetupExample.java
+++ jdollarx-example/src/test/java/com/github/loyada/jdollarxexample/MeetupExample.java
@@ -1,6 +1,7 @@
 package com.github.loyada.jdollarxexample;
 
-import com.github.loyada.jdollarx.InBrowser;
+import com.github.loyada.jdollarx.singlebrowser.InBrowserSinglton;
+import static com.github.loyada.jdollarx.singlebrowser.InBrowserSinglton.*;
 import com.github.loyada.jdollarx.Operations;
 import com.github.loyada.jdollarx.Path;
 
@@ -10,22 +11,23 @@ import static com.github.loyada.jdollarx.ElementProperties.*;
 
 public class MeetupExample {
       public static void main(String[] argc) throws Operations.OperationFailedException {
-          InBrowser browser = new InBrowser(DriverSetup.createStandardChromeDriver());
-          browser.getDriver().get("http://www.meetup.com");
+          InBrowserSinglton.driver = DriverSetup.createStandardChromeDriver();
+          InBrowserSinglton.driver.get("http://www.meetup.com");
 
           Path techCategory = anchor.withClass("categoryLink").that(hasAggregatedTextContaining("Tech"));
-          browser.clickAt(techCategory);
+          scrollTo(techCategory);
+          clickAt(techCategory);
 
           Path navigationBar = div.that(hasId("findNavBar")).describedBy("navigation bar");
           Path searchInput = input.that(hasAttribute("name", "keywords")).inside(navigationBar);
-          browser.sendKeys("java NYC\n").to(searchInput);
+          sendKeys("java NYC\n").to(searchInput);
 
           Path nycJavaCard = listItem.withClass("groupCard").that(hasAggregatedTextContaining("new york city Java meetup group"));
-          browser.clickAt(nycJavaCard);
+          clickAt(nycJavaCard);
 
-          browser.clickAt(anchor.withText("timothy fagan"));
-          browser.clickAt(anchor.inside(span.that(hasId("member-profile-photo"))));
+          clickAt(anchor.withText("timothy fagan"));
+          clickAt(anchor.inside(span.that(hasId("member-profile-photo"))));
 
-          browser.getDriver().quit();
+          InBrowserSinglton.driver.quit();
     }
 }

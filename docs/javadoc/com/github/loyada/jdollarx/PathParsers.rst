ae43060a450501510f0cdc923f90991ce5e229df
Danny Loya <dan.loya@gmail.com>
2018-03-11 23:15:51 -0400

    documentation -WIP

--- docs/javadoc/com/github/loyada/jdollarx/PathParsers.rst
+++ docs/javadoc/com/github/loyada/jdollarx/PathParsers.rst
@@ -26,7 +26,16 @@ PathParsers
 
 .. java:type:: public final class PathParsers
 
-   Internal implementation.
+   functions to find DOM elements in a W3C document. These functions are also useful to experiment and test with how Paths can be used to extract elements (they are used in many of the unit tests of DollarX).
+
+   .. parsed-literal::
+
+      Example use:
+      Path el = div.before(span);
+          String xpath = el.getXPath().get();
+          NodeList nodes = findAllByXpath("<div>foo</div><div>boo</div><span></span>", el);
+          assertThat(nodes.getLength(), is(2));
+          assertThat(nodes.item(0).getTextContent(), equalTo("foo"));
 
 Methods
 -------
@@ -36,11 +45,23 @@ findAllByPath
 .. java:method:: public static NodeList findAllByPath(String docString, Path path) throws XPathExpressionException, IOException, SAXException, ParserConfigurationException
    :outertype: PathParsers
 
+   find all the nodes that match a path in a W3C document
+
+   :param docString: a W3C document
+   :param path: the path to find.
+   :return: a node list with the details of all the elements that match the given path
+
 findAllByPath
 ^^^^^^^^^^^^^
 
 .. java:method:: public static NodeList findAllByPath(Document doc, Path path) throws XPathExpressionException
    :outertype: PathParsers
+
+   find all the nodes that match a path in a W3C document
+
+   :param doc: a W3C document
+   :param path: the path to find
+   :return: a node list with the details of all the elements that match the given path
 
 findAllByXPath
 ^^^^^^^^^^^^^^
@@ -48,9 +69,20 @@ findAllByXPath
 .. java:method:: public static NodeList findAllByXPath(Document doc, String extractedXpath) throws XPathExpressionException
    :outertype: PathParsers
 
+   internal implementation
+
+   :param doc: a W3C document
+   :param extractedXpath: an xpath
+   :return: a node list with the details of all the elements that match the given xpath
+
 getDocumentFromString
 ^^^^^^^^^^^^^^^^^^^^^
 
-.. java:method:: public static Document getDocumentFromString(String exampleString) throws ParserConfigurationException, IOException, SAXException
+.. java:method:: public static Document getDocumentFromString(String document) throws ParserConfigurationException, IOException, SAXException
    :outertype: PathParsers
+
+   Convert a string to a \ :java:ref:`Document`\ , Assuming utf-8 encoding.
+
+   :param document: the document as a string
+   :return: the document as a @link Document}
 
--- jdollarx/src/main/java/com/github/loyada/jdollarx/PathParsers.java
+++ jdollarx/src/main/java/com/github/loyada/jdollarx/PathParsers.java
@@ -15,26 +15,59 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 /**
- * Internal implementation.
+ * functions to find DOM elements in a W3C document. These functions are also useful to experiment and test with how Paths
+ * can be used to extract elements (they are used in many of the unit tests of DollarX).
+ * <pre>Example use:
+ * {@code
+ *     Path el = div.before(span);
+ *     String xpath = el.getXPath().get();
+ *     NodeList nodes = findAllByXpath("<div>foo</div><div>boo</div><span></span>", el);
+ *     assertThat(nodes.getLength(), is(2));
+ *     assertThat(nodes.item(0).getTextContent(), equalTo("foo"));
+ * }
+ * </pre>
  */
 public final class PathParsers {
     private PathParsers(){}
 
-    public static Document getDocumentFromString(final String exampleString) throws ParserConfigurationException, IOException, SAXException {
+    /**
+     * Convert a string to a {@link Document}, Assuming utf-8 encoding.
+     * @param document the document as a string
+     * @return the document as a @link Document}
+     */
+    public static Document getDocumentFromString(final String document) throws ParserConfigurationException, IOException, SAXException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
-        InputStream input = new ByteArrayInputStream(exampleString.getBytes(StandardCharsets.UTF_8));
+        InputStream input = new ByteArrayInputStream(document.getBytes(StandardCharsets.UTF_8));
         return builder.parse(input);
     }
 
+    /**
+     * find all the nodes that match a path in a W3C document
+     * @param docString a W3C document
+     * @param path the path to find.
+     * @return a node list with the details of all the elements that match the given path
+     */
     public static NodeList findAllByPath(final String docString, final Path path) throws XPathExpressionException, IOException, SAXException, ParserConfigurationException {
         return findAllByPath(getDocumentFromString(docString), path );
     }
 
+    /**
+     * find all the nodes that match a path in a W3C document
+     * @param doc a W3C document
+     * @param path the path to find
+     * @return a node list with the details of all the elements that match the given path
+     */
     public static NodeList findAllByPath(final Document doc, final Path path) throws XPathExpressionException {
        return findAllByXPath(doc, path.getXPath().get());
     }
 
+    /**
+     * internal implementation
+     * @param doc a W3C document
+     * @param extractedXpath an xpath
+     * @return a node list with the details of all the elements that match the given xpath
+     */
     public static NodeList findAllByXPath(final Document doc, final String extractedXpath) throws XPathExpressionException {
         XPathFactory xPathfactory = XPathFactory.newInstance();
         XPath xpath = xPathfactory.newXPath();

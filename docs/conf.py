1628636b8aabd0b945dbaf535181a83299d90d5d
Danny Loya <dan.loya@gmail.com>
2018-05-07 08:27:28 -0400

    v0.37

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Danny Loya'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.36'
+release = '0.37'
 
 
 # -- General configuration ---------------------------------------------------

4ca97bf81d62012b42a701c1502a0524514ece27
Danny Loya <dan.loya@gmail.com>
2018-11-26 10:50:41 -0500

    doc update

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Danny Loya'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.38'
+release = '0.39'
 
 
 # -- General configuration ---------------------------------------------------

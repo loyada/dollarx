6c64448b94407c70d71988e5a17659ee9402d0b0
Danny Loya <danny.loya@iongroup.com>
2019-05-12 21:01:59 -0400

    v0.62

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Danny Loya'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.61'
+release = '0.62'
 
 
 # -- General configuration ---------------------------------------------------

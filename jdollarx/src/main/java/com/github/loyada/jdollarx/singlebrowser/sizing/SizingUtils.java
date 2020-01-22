d6985e92138c34e27d095da5c31ff18aa7ec907f
loyada <dan.loya@gmail.com>
2020-01-22 02:06:01 -0500

    fix a bug in SizingUtils - driver was not updated, thus consecutive tests were breaking

--- jdollarx/src/main/java/com/github/loyada/jdollarx/singlebrowser/sizing/SizingUtils.java
+++ jdollarx/src/main/java/com/github/loyada/jdollarx/singlebrowser/sizing/SizingUtils.java
@@ -14,7 +14,12 @@ class SizingUtils {
     static final String HEIGHT = "height";
     static final String WIDTH = "width";
 
-    private static JavascriptExecutor js = (JavascriptExecutor) driver;
+    /*
+      NOTE: do not refactor the assignment to js (i.e. JavascriptExecutor js = (JavascriptExecutor) driver;) to be static. This will break tests.
+      The reason is that if there are 2 tests being ran serially, the first one quits the driver and the second one
+      starts a new driver. But if the assignment to the "js" variable is done only once, it will keep referring to the dead driver, and the
+      operations done on it will fail.
+     */
 
     static void setDimensions(Path path, int width, int height) {
         setDimensions(path, format("%dpx", width), format("%dpx", height));
@@ -24,24 +29,27 @@ class SizingUtils {
         return format("'%s'", s);
     }
     static  void setDimensions(Path path, String width, String height) {
-
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         WebElement el = find(path);
         String script = format("arguments[0].style.width=%s;  arguments[0].style.height=%s;", correct(width), correct(height));
         js.executeScript(script, el);
     }
 
     static Map<String, Long> getVisibleDimensions(Path path) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         WebElement el = find(path);
         return castToMap(js.executeScript("return  { 'height':  arguments[0].clientHeight, 'width':   arguments[0].clientWidth};", el));
     }
 
 
     static Map<String, Long> getScrollableDimensions(Path path) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         WebElement el = find(path);
         return castToMap(js.executeScript("return  { 'height':  arguments[0].scrollHeight, 'width':   arguments[0].scrollWidth};", el));
     }
 
     static Map<String, String> getStylingDimensions(Path path) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         WebElement el = find(path);
         return castToMap(js.executeScript("return  { 'height':  arguments[0].style.height, 'width':   arguments[0].style.width};", el));
     }
---
+++ jdollarx/src/main/java/com/github/loyada/jdollarx/singlebrowser/sizing/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Custom manipulations and evaluations of dimensions of elements or browser window size - for a singleton instance of browser
+ */
+package com.github.loyada.jdollarx.singlebrowser.sizing;

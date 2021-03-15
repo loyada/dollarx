82c82ef6d90e47d9810b1037da1ba4b8c8c92261
loyada <dan.loya@gmail.com>
2021-03-14 21:45:27 -0400

    doc

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Danny Loya'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.10'
+release = '1.2.0'
 
 
 # -- General configuration ---------------------------------------------------

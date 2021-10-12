616492037ceff03064faf5c05d8dbe46ba63e44b
loyada <dan.loya@gmail.com>
2021-10-11 22:31:06 -0400

    dockerfile

--- jdollarx/src/test/java/com/github/loyada/jdollarx/aggrid/GridMenuOperationsIntegration.java
+++ jdollarx/src/test/java/com/github/loyada/jdollarx/aggrid/GridMenuOperationsIntegration.java
@@ -6,13 +6,16 @@ import com.github.loyada.jdollarx.Path;
 import com.github.loyada.jdollarx.singlebrowser.AgGrid;
 import com.github.loyada.jdollarx.singlebrowser.AgGridHighLevelOperations;
 import com.github.loyada.jdollarx.singlebrowser.InBrowserSinglton;
+import com.github.loyada.jdollarx.singlebrowser.TemporaryChangedTimeout;
 import com.github.loyada.jdollarx.singlebrowser.custommatchers.AgGridMatchers;
 import com.github.loyada.jdollarx.singlebrowser.custommatchers.CustomMatchers;
+import com.github.loyada.jdollarx.singlebrowser.highlevelapi.Inputs;
 import org.hamcrest.Matchers;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.NoSuchElementException;
 
 import java.util.AbstractMap.SimpleEntry;
 import java.util.Arrays;
@@ -24,6 +27,7 @@ import static com.github.loyada.jdollarx.BasicPath.button;
 import static com.github.loyada.jdollarx.BasicPath.div;
 import static com.github.loyada.jdollarx.ElementProperties.hasAggregatedTextEqualTo;
 import static com.github.loyada.jdollarx.ElementProperties.hasId;
+import static com.github.loyada.jdollarx.ElementProperties.hasText;
 import static com.github.loyada.jdollarx.singlebrowser.AgGrid.HEADER_CELL;
 import static com.github.loyada.jdollarx.singlebrowser.AgGrid.SortDirection.ascending;
 import static com.github.loyada.jdollarx.singlebrowser.AgGrid.SortDirection.descending;
@@ -34,6 +38,8 @@ import static com.github.loyada.jdollarx.singlebrowser.InBrowserSinglton.find;
 import static com.github.loyada.jdollarx.singlebrowser.InBrowserSinglton.findAll;
 import static com.github.loyada.jdollarx.singlebrowser.custommatchers.CustomMatchers.isPresent;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.fail;
 
 public class GridMenuOperationsIntegration {
     private AgGrid grid;
@@ -68,10 +74,11 @@ public class GridMenuOperationsIntegration {
                 .withRowsAsStringsInOrder(Arrays.asList(row1, row2))
                 .containedIn(div.that(hasId("myGrid")))
                 .build();
-        try{
-            clickOn(button.that(hasAggregatedTextEqualTo("accept all cookies")));
-        } catch (Exception e) {
-            // no such button
+
+        try(TemporaryChangedTimeout timeout = new TemporaryChangedTimeout(10, TimeUnit.SECONDS)) {
+                clickOn(button.withText("accept all cookies"));
+        } catch (Exception ex) {
+                // no such button
         }
     }
 

89f8d9c1738fc5b1d80c34dac5bffa0e306879cd
loyada <loyada@users.noreply.github.com>
2022-05-19 09:48:02 -0400

    scaling

--- jdollarx/src/main/java/com/github/loyada/jdollarx/visual/Images.java
+++ jdollarx/src/main/java/com/github/loyada/jdollarx/visual/Images.java
@@ -12,6 +12,8 @@ import org.openqa.selenium.WebElement;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.Image;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -298,15 +300,29 @@ public class Images {
     }
   }
 
+  
+  public static BufferedImage resize(BufferedImage img, int newW, int newH) {
+        Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+        BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2d = dimg.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+
+        return dimg;
+   }
+  
   public static BufferedImage captureImage(InBrowser browser, Path el) {
     WebElement webEl = browser.find(el);
     File screenshot = ((TakesScreenshot) browser.getDriver()).getScreenshotAs(OutputType.FILE);
-    final BufferedImage fullImg;
+    final BufferedImage fullImgUnscaled;
     try {
-      fullImg = ImageIO.read(screenshot);
+            fullImgUnscaled = ImageIO.read(screenshot);
     } catch (IOException e) {
-      throw new RuntimeException(e);
+            throw new RuntimeException(e);
     }
+    Dimension fullSize = InBrowserSinglton.find(html).getSize();
+    BufferedImage fullImg = resize(fullImgUnscaled, fullSize.getWidth(), fullSize.getHeight());
     Point elementLocation = webEl.getLocation();
     Dimension elementDimensions = webEl.getSize();
 
